9a0fd9cd58e7f680edaa920519fb9e07cedc2753
garyxdh <garyxdh@gmail.com>
2016-11-10 10:21:20 +0800

    draft of demo 1.0
    添加app类和render方法

--- 4_react-native-todo-list/react-todo-list-master/components/App.js
+++ 4_react-native-todo-list/react-todo-list-master/components/App.js
@@ -1,1 +1,28 @@
 
+import React, { Component } from 'react'
+//var React = require('react');
+//var Component = require('react').Component;
+
+class App extends Component{
+//var App = React.createClass({
+    //render: funcion(){
+     render() {
+        /*return (
+            <div>This def a React app now</div>
+        );*/
+        return <div>This def a React app now</div>
+    }
+
+}
+//});
+
+//module.export = App;
+export default App
+
+
+/*
+    //"react": "^0.14.7",
+    "react": "^15.3.2",
+    //"react-dom": "^0.14.8",
+    "react-dom": "^15.3.2",
+    */
